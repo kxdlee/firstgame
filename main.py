1ac208f67ee11edca37e216a4e60865e922e51cb
Vadim Ustinov <ustinovvadim842@gmail.com>
2021-03-21 19:02:27 +0300

    added player

--- main.py
+++ main.py
@@ -11,12 +11,25 @@ class Object(pygame.sprite.Sprite):
         self.rect.y = y
         self.speed = speed
 
-width = 500
-height = 500
+width = 800
+height = 600
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("krutaya igrulya")
 
+# точка спавна игрока
+start_x = 100
+start_y = 120
+
+#импорт изображений
 bg = pygame.transform.scale(pygame.image.load("images/bg.png"),(width, height))
+player_img = pygame.transform.scale(pygame.image.load("images/player.png"),(54, 54))
+
+# создание групп объектов
+all_sprites = pygame.sprite.Group()
+
+#создание объектов
+player = Object(player_img, start_x, start_y, 1)
+all_sprites.add(player)
 
 run = True
 
@@ -27,13 +40,24 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    keys = pygame.key.get_pressed()
+    if event.type == pygame.KEYDOWN:
+        if keys[pygame.K_w]:
+            player.rect.y -= player.speed
+        if keys[pygame.K_s]:
+            player.rect.y += player.speed
+        if keys[pygame.K_a]:
+            player.image = pygame.transform.flip(player_img, True, False)
+            player.rect.x -= player.speed
+        if keys[pygame.K_d]:
+            player.image = player_img
+            player.rect.x += player.speed
 
 
 
 
 
 
-
-
-
+    all_sprites.draw(window)
+    all_sprites.update()
     pygame.display.update()
